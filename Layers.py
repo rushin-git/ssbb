187e45bedf588559544c7cc50aa4ffe455d4e9a0
ShinRu <1737490679@qq.com>
2021-02-26 17:31:27 +0800

    Add files via upload

--- Layers.py
+++ Layers.py
@@ -7,16 +7,16 @@ from keras.regularizers import l2
 
 
 ##KWPE Layer
+
 class KWPE(Layer):
-    def __init__(self,bias=True,sr=10,unit=16,**kwargs):
+    def __init__(self,bias=True,unit=16,**kwargs):
+        # self.output_dim = output_dim
         self.supports_masking = True
         super(KWPE, self).__init__(**kwargs)
         self.bias=bias
-        self.sr=sr
         self.unit=unit
     def build(self, input_shape):
         assert isinstance(input_shape, list)
-        # 为该层创建一个可训练的权重
         self.w1 =self.add_weight(name='w1',
                                       shape=(input_shape[0][-1],self.unit),
                                       initializer='glorot_normal',
@@ -39,18 +39,21 @@ class KWPE(Layer):
                                       initializer='glorot_normal',
                                       regularizer=l2(0.000001), 
                                        trainable=True)
-        super(KWPE, self).build(input_shape)
+        super(KWPE, self).build(input_shape)  # 一定要在最后调用它
     def compute_mask(self, input, input_mask=None):
+        # need not to pass the mask to next layers
         return None
     def call(self,x,mask=None):
         assert isinstance(x, list)
-        x1,x2=x
+        x1,x2,srs=x
+        sr=srs[0]
         input_shape = K.shape(x1)
         pq=K.batch_dot(x1,x2,axes=2)
         pp=K.batch_dot(x1,x1,axes=2)
         qp=K.batch_dot(x2,x1,axes=2)
         qq=K.batch_dot(x2,x2,axes=2)
-        eij1 = K.dot(K.tanh(K.dot(pq,self.w2)+K.dot(pp,self.w3)+K.dot(x1,self.w1)),self.we)*self.sr
+        eij1 = K.dot(K.tanh(K.dot(pq,self.w2)+K.dot(pp,self.w3)+K.dot(x1,self.w1)),self.we)*sr
+        # k = K.expand_dims(K.dot(x2, self.w2)+self.b2, 1)
         ai1 = K.exp(eij1)
         if mask!=None:
             ms1=tf.cast(mask[0],'float32')
@@ -58,8 +61,11 @@ class KWPE(Layer):
             ai1 = ai1*ms1
         
         ww1 = ai1/(K.sum(ai1, axis=1,keepdims=True)+ K.epsilon())
+        
         ot1 = x1*ww1
-        eij2 = K.dot(K.tanh(K.dot(qp,self.w2)+K.dot(qq,self.w3)+K.dot(x2,self.w1)),self.we)*self.sr
+        # e2 = (e1 - K.min(e2, axis=1, keepdims=True))/(K.max(e2, axis=1, keepdims=True)-K.min(e2, axis=1, keepdims=True)+ K.epsilon())
+        eij2 = K.dot(K.tanh(K.dot(qp,self.w2)+K.dot(qq,self.w3)+K.dot(x2,self.w1)),self.we)*sr
+        # eij2 = *self.sr
         ai2 = K.exp(eij2)
         if mask!=None:
             ms2=tf.cast(mask[1],'float32')
@@ -67,14 +73,19 @@ class KWPE(Layer):
             ai2 = ai2*ms2
         ww2 = ai2/(K.sum(ai2, axis=1,keepdims=True)+ K.epsilon())
         ot2 = x2*ww2
+        # return weighted_input.sum(axis=1)
+        # oot1=K.sum(ot1,axis=1)
+        # oot2=K.sum(ot2,axis=1)
+        
         oot1=K.sum(ot1,axis=1)
         oot2=K.sum(ot2,axis=1)
+        # return [ww1,ww2]
         return [ww1,ww2,ot1,ot2,oot1,oot2]
     def compute_output_shape(self, input_shape):
         assert isinstance(input_shape, list)
-        shape_a, shape_b = input_shape
+        shape_a, shape_b,shape_c = input_shape
         return [(shape_a[0],shape_a[1],1), (shape_b[0],shape_b[1],1),(shape_a[0],shape_a[1],shape_a[2]), (shape_b[0],shape_b[1],shape_b[2]),(shape_a[0],shape_a[2]), (shape_b[0],shape_b[2])]
-
+        # return [(shape_a[0],shape_a[1],1), (shape_b[0],shape_b[1],1)]
 
 class MaskLayer(Layer):
     def __init__(self,**kwargs):
